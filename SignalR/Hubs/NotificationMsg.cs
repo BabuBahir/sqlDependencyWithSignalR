0b402a599db1385e349589154001d19e9dd1e747
Administrator <Administrator@SAYAK>
2018-04-20 19:48:12 +0530

    some changes

--- SignalR/Hubs/NotificationMsg.cs
+++ SignalR/Hubs/NotificationMsg.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNet.SignalR;
 using SignalR.Models;
 using GenericRepository;
 using SignalR.Service;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SignalR.Hubs
 {
@@ -17,7 +19,7 @@ namespace SignalR.Hubs
             Clients.All.NewMessage(msgs);
         }
 
-        public void GetNotificationMsg()
+        public void GetNotificationMsg(string name , string msg)
         {
             List<NotificationDetails> msgs = NotificationService.GetAllNotificationDetails();
             Clients.All.NewMessage(msgs);
@@ -37,6 +39,44 @@ namespace SignalR.Hubs
             //}
         }
 
-        
+        public override Task OnConnected()
+        {             
+            UserHandler.chatusers.Add(new ChatUser { name = "", connID = Context.ConnectionId.ToString() });
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {             
+            UserHandler.chatusers.RemoveAll(x => x.connID == Context.ConnectionId.ToString());
+            var jsonObj = JsonConvert.SerializeObject(UserHandler.chatusers);
+            Clients.Caller.updateActiveUsers(jsonObj);
+
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public async void saveUserName(string name, string connID)
+        {
+            var index = UserHandler.chatusers.FindIndex(x => x.connID == connID);
+            UserHandler.chatusers.ElementAt(index).name = name;
+
+            var jsonObj = JsonConvert.SerializeObject(UserHandler.chatusers);
+            await Clients.All.updateActiveUsers(jsonObj);
+
+        }
+
+
     }
+
+    public static class UserHandler
+    {         
+        public static List<ChatUser> chatusers = new List<ChatUser>();
+    }
+
+    public class ChatUser
+    {
+        public string name { get; set; }
+        public string connID { get; set; }
+    }
+
 }
